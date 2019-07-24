0f2ac59042519451f5a3b403f84ae23532e52394
tangtonglong <532697721@qq.com>
2019-07-24 19:36:23 +0800

    修改配置

--- paascloud-discovery/src/main/java/com/paascloud/discovery/PaasCloudDiscoveryApplication.java
+++ paascloud-discovery/src/main/java/com/paascloud/discovery/PaasCloudDiscoveryApplication.java
@@ -12,6 +12,7 @@
 package com.paascloud.discovery;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -23,6 +24,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @SpringBootApplication
 @EnableConfigServer
+//@EnableAutoConfiguration
 @EnableDiscoveryClient
 public class PaasCloudDiscoveryApplication {
 
--- paascloud-eureka/src/main/java/com/paascloud/discovery/eureka/PaasCloudEurekaApplication.java
+++ paascloud-eureka/src/main/java/com/paascloud/discovery/eureka/PaasCloudEurekaApplication.java
@@ -14,6 +14,9 @@ package com.paascloud.discovery.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * The class Paas cloud eureka application.
@@ -31,5 +34,24 @@ public class PaasCloudEurekaApplication {
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(PaasCloudEurekaApplication.class, args);
+	}
+
+	/**
+	 * 2.0版本的security默认加上了 csrf 拦截, 所以需要通过重写方法, 把csrf拦截禁用
+	 * 参考: https://github.com/yinjihuan/spring-cloud/issues/1
+	 * <pre>
+	 *     This is because @EnableWebSecurity is now added by default when Spring Security is on the classpath.
+	 *     This enable CSRF protection by default. You will have the same problem in 1.5.10 if you add @EnableWebSecurity.
+	 *     One work around, which is not the most secure workaround if you have browsers using the Eureka dashboard, is to disable CSRF protection.
+	 *     This can be done by adding the following configurations to your app.
+	 * </pre>
+	 */
+	@EnableWebSecurity
+	static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable();
+			super.configure(http);
+		}
 	}
 }
